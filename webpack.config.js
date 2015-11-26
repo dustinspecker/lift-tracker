1b6bb62efb923a2300aa32957cd21dc1cf56b225
Dustin Specker <dustinspecker@dustinspecker.com>
2015-11-26 15:38:09 -0600

    chore(webpack): setup babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,15 @@ module.exports = {
   entry: [
     './src/index.js'
   ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
