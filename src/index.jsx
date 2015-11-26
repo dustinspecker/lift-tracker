6ccb862a141ea5ec510db2503fe6f16bbe45acdd
Dustin Specker <dustinspecker@dustinspecker.com>
2015-11-26 15:56:04 -0600

    feat(components): create lift-label

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,11 @@
-console.log('hello');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import LiftLabel from './components/lift-label';
+
+const name = 'squat';
+
+ReactDOM.render(
+  <LiftLabel name={name} />,
+  document.getElementById('app')
+);
