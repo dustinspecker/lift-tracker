ab0e099f6e8a4e6aec90763073070e253bcae7a1
Dustin Specker <dustinspecker@dustinspecker.com>
2015-11-27 17:05:05 -0600

    refactor(reducer): add lift only takes lifts list

--- src/reducer.js
+++ src/reducer.js
@@ -4,22 +4,14 @@ const INITIAL_STATE = Map({
   lifts: List()
 });
 
-function addLift(state, name, weight = 45) {
-  const existingLift = (
-    state
-      .get('lifts', List())
-      .find(lift => lift.get('name') === name)
-  );
+function addLift(lifts, name, weight = 45) {
+  const existingLift = lifts.find(lift => lift.get('name') === name);
 
   if (existingLift) {
-    return state;
+    return lifts;
   }
 
-  return state.update(
-   'lifts',
-    List(),
-    lifts => lifts.push(Map({name, weight}))
-  );
+  return lifts.push(Map({name, weight}))
 }
 
 function removeLift(state, index) {
@@ -56,7 +48,11 @@ function incrementWeight(state, index) {
 export default function (state = INITIAL_STATE, action = {}) {
   switch (action.type) {
     case 'ADD_LIFT':
-      return addLift(state, action.name, action.weight);
+      return state.update(
+        'lifts',
+        List(),
+        lifts => addLift(lifts, action.name, action.weight)
+      );
     case 'DECREMENT_WEIGHT':
       return decrementWeight(state, action.index);
     case 'INCREMENT_WEIGHT':
