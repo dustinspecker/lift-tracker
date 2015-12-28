02d98c0b557f5daf874f798cd4fd0806954cc7c1
Dustin Specker <dustinspecker@dustinspecker.com>
2015-12-28 17:12:17 -0600

    refactor(LiftList): use arrow functions and destructuring

--- src/components/lift-list.jsx
+++ src/components/lift-list.jsx
@@ -4,23 +4,16 @@ import React from 'react';
 import LiftLabel from './lift-label';
 import Weight from './weight';
 
-const LiftList = React.createClass({
-  render() {
-    return <div>
-      {this.props.lifts.map(lift =>
-        <div key={lift.name}>
-          <LiftLabel name={lift.name} />
-          <Weight weight={lift.weight} />
-        </div>
-      )}
-    </div>;
-  }
-});
+const LiftList = ({lifts}) =>
+  <div>
+    {lifts.map(lift =>
+      <div key={lift.name}>
+        <LiftLabel name={lift.name} />
+        <Weight weight={lift.weight} />
+      </div>
+    )}
+  </div>;
 
-function mapStateToProps(state) {
-  return {
-    lifts: state.get('lifts').toJS()
-  };
-}
+const mapStateToProps = state => ({lifts: state.get('lifts').toJS()});
 
 export const LiftListContainer = connect(mapStateToProps)(LiftList);
