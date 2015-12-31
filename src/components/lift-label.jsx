918e1a553620bee2af3077c83f8dac88e064b68e
Dustin Specker <dustinspecker@dustinspecker.com>
2015-12-30 20:45:07 -0600

    chore(LiftLabel): add propTypes

--- src/components/lift-label.jsx
+++ src/components/lift-label.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export default React.createClass({
+  propTypes: {
+    name: React.PropTypes.string.isRequired
+  },
   render() {
     return <div>{this.props.name}</div>
   }
