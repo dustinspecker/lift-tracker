8d02f0cf0d815ff1def38c3b4bd517e1059d0277
Dustin Specker <dustinspecker@dustinspecker.com>
2015-12-30 22:44:16 -0600

    chore(webpack): create production config

---
+++ webpack-production.config.babel.js
@@ -0,0 +1,40 @@
+import webpack from 'webpack'
+
+export default {
+  entry: [
+    './src/index.jsx'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.PrefetchPlugin('react'),
+    new webpack.PrefetchPlugin('react/lib/ReactComponentBrowserEnvironment'),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.NoErrorsPlugin()
+  ]
+}
