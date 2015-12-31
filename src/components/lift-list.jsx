17f5eecef5c11502292767dfa6bcd23ec0f5bfeb
Dustin Specker <dustinspecker@dustinspecker.com>
2015-12-30 21:18:51 -0600

    feat(LiftList): use pure render mixin
    Converted LiftList to createClass to use mixin. Cannot currently
    add mixins to function components.

--- src/components/lift-list.jsx
+++ src/components/lift-list.jsx
@@ -1,25 +1,31 @@
 import {connect} from 'react-redux'
 import ImmutablePropTypes from 'react-immutable-proptypes'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
 import React from 'react'
 
 import {decrementWeight, incrementWeight} from '../actions'
 import LiftLabel from './lift-label'
 import Weight from './weight'
 
-const LiftList = ({decrement, increment, lifts}) =>
-  <div>
-    {lifts.toJS().map((lift, index) =>
-      <div key={lift.name}>
-        <LiftLabel name={lift.name} />
-        <Weight
-          decrement={decrement}
-          increment={increment}
-          index={index}
-          weight={lift.weight}
-        />
-      </div>
-    )}
-  </div>
+const LiftList = React.createClass({
+  mixins: [PureRenderMixin],
+  render() {
+    const {decrement, increment, lifts} = this.props
+    return <div>
+      {lifts.toJS().map((lift, index) =>
+        <div key={lift.name}>
+          <LiftLabel name={lift.name} />
+          <Weight
+            decrement={decrement}
+            increment={increment}
+            index={index}
+            weight={lift.weight}
+          />
+        </div>
+      )}
+    </div>
+  }
+})
 
 LiftList.propTypes = {
   decrement: React.PropTypes.func.isRequired,
